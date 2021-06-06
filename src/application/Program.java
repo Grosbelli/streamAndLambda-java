277c3f28d016cf4f3fe2f01a65c952553c8f7660
Andrey Grosbelli <agrosbelli@yahoo.com.br>
2021-06-06 16:49:34 -0300

    Depois do refactory aplicado, vendo a resposta do professor

--- src/application/Program.java
+++ src/application/Program.java
@@ -13,52 +13,43 @@ public class Program {
 
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-        List<Employee> employees = new ArrayList<>();
 
         System.out.print("Enter full file path: ");
         String path = sc.next();
 
         try(BufferedReader br = new BufferedReader(new FileReader(path))) {
 
+            List<Employee> employees = new ArrayList<>();
+
             String line = br.readLine();
             while (line != null) {
                 String[] fields = line.split(",");
                 employees.add(new Employee(fields[0], fields[1], Double.parseDouble(fields[2])));
-
                 line = br.readLine();
             }
+
+            System.out.print("Enter salary: ");
+            Double salary = sc.nextDouble();
+
+            List<String> emails = employees.stream()
+                    .filter(emp -> emp.getSalary() > salary)
+                    .map(emp -> emp.getEmail()).sorted()
+                    .collect(Collectors.toList());
+
+            System.out.println("Email of people whose salary is more than 2000.00: ");
+            emails.forEach(System.out::println);
+
+            double sum = employees.stream()
+                    .filter(emp -> emp.getName().charAt(0) == 'M')
+                    .map(emp -> emp.getSalary())
+                    .reduce(0.0, (x,y) -> x + y);
+
+            System.out.println("\nSum of salary of people whose name starts with 'M': " + String.format("%.2f", sum));
+
 
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        System.out.print("Enter salary: ");
-        Double salary = sc.nextDouble();
-        System.out.println("Email of people whose salary is more than 2000.00: ");
-
-        Comparator<String> comp = (e1, e2) -> e1.toUpperCase().compareTo(e2.toUpperCase());
-
-        List<String> highSalary = employees.stream()
-                .filter(emp -> emp.getSalary() > salary)
-                .map(emp -> emp.getEmail()).sorted(comp)
-                .collect(Collectors.toList());
-
-        highSalary.forEach(emp -> {
-            System.out.println(emp);
-        });
-
-        System.out.println("Sum of salary of people whose name starts with 'M': ");
-        List<Double> sumSalary = employees.stream()
-                .filter(emp -> emp.getName().charAt(0) == 'M')
-                .map(emp -> emp.getSalary())
-                .collect(Collectors.toList());
-
-        Double sum = 0.0;
-        for (Double e : sumSalary) {
-            sum += e;
-        }
-
-        System.out.print(sum);
 
         sc.close();
     }
